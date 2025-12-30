537d8309edf91d7a451a396e2b3c9c3154e9e1ab
jiangxiaoyong <jam.xylitol@gmail.com>
2025-12-30 14:33:37 +0800

    feat: add Prettier configuration and update dependencies

--- site/.vitepress/theme/index.ts
+++ site/.vitepress/theme/index.ts
@@ -1,13 +1,13 @@
-import { Underline } from "@theojs/lumen";
-import type { EnhanceAppContext } from "vitepress";
-import DefaultTheme from "vitepress/theme";
+import { Underline } from '@theojs/lumen';
+import type { EnhanceAppContext } from 'vitepress';
+import DefaultTheme from 'vitepress/theme';
 
-import "@theojs/lumen/style";
-import "./styles.css";
+import '@theojs/lumen/style';
+import './styles.css';
 
 export default {
   extends: DefaultTheme,
   enhanceApp: ({ app }: EnhanceAppContext) => {
-    app.component("Home", Underline);
-  },
+    app.component('Home', Underline);
+  }
 };
